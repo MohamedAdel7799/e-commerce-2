014d02038604e8dfb912e6cbb2f3a00147edc25e
mohamed adel <ahmrn1999@gmail.com>
2024-02-01 02:46:39 +0200

    edit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter } from 'react-router-dom';
-import Authcontext from './pages/authenticationcontext/authcontext';
 
 
 
