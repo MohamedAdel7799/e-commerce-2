4ed676b390ffcbe3df99265eec9b57569af35b4e
mohamed adel <ahmrn1999@gmail.com>
2024-02-14 09:07:20 +0200

    edit

--- src/pages/shoppingcart/shopingcart.jsx
+++ src/pages/shoppingcart/shopingcart.jsx
@@ -9,17 +9,70 @@ function Cart(){
     return(
 
          <div> 
-            {cartitems.map((e)=>{
+            {cartitems.length==0?(<div className=" vh-100 d-grid  ">
+                <div className="m-auto h-75 w-75 bg-light shadow-lg  ">
+                  <div className="w-100 p-4 bg-info rounded-top-5  d-flex justify-content-between ">
+                    <div className="fw-bolder mx-4">
+                        shopping cart
+                    </div>
+                    <div className="fw-bolder mx-4">icon</div>
+                  </div>
+                </div>
+
+            </div>):(
+            <div className="w-75 mx-auto bg-light shadow-lg my-5 ">
+                 <div className="w-100 p-4 bg-info rounded-top-5  d-flex justify-content-between ">
+                    <div className="fw-bolder mx-4">
+                        shopping cart
+                    </div>
+                    <div className="fw-bolder mx-4">icon</div>
+                  </div>
+                <div className="w-100 my-5 ">{cartitems.map((e)=>{
                 return (
-                    <div>
-                        <h1>{e.title}</h1>
-                        <h2>{e.price}</h2>
+                    <div className=" container w-100 my-5  ">
+                        <div>
+                            <ul>
+                                <li className=" list-unstyled ">
+                                    <div className=" py-3 d-flex justify-content-between">
+                                        <div className=" d-flex  ">
+                                            <img src={e.image} className=" img-fluid " style={{width:'130px', height:"150"}}></img>
+                                            <div className=" my-auto mx-5  ">
+                                                <p>{e.title}</p>
+                                                <p>{e.price}</p>
+                                            </div>
+                                        </div>
+                                        <div className="text-center mx-3 ">
+                                            <div className="d-flex justify-content-around ">
+                                                <button className="btn btn-danger"> + </button>
+                                                <button className="btn btn-danger"> - </button>
+                                            </div>
+                                            <div className="">
+                                            <button className="btn btn-info my-3"> remove </button>
+
+
+                                            </div>
+
+                                        </div>
+
+                                       
+                                    </div>
+                                </li>
+                            </ul>
+
+                        </div>
+                        
 
                     </div>
                 )
 
 
-            })}
+            })}</div>
+
+
+
+
+            </div>) }
+           
            
                    
         </div>
